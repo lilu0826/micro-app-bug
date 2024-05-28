cc2bb1566e00aa8e7754fb825c4a2b8b98e1237f
lilu <lilu@bigdatacd.com>
2024-05-28 10:08:11 +0800

    code

---
+++ main-app/src/main.js
@@ -0,0 +1,15 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import microApp from '@micro-zoe/micro-app'
+
+const app = createApp(App)
+
+
+microApp.start()
+
+app.use(router)
+
+app.mount('#app')
---
+++ main-app/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+    <micro-app
+        name="my-app"
+        url="http://localhost:3001/"
+        router-mode="native"
+        baseroute="/sub-app/"
+        iframe
+        style="display: block;height: 100%;"
+    ></micro-app>
+</template>
